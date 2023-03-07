f4fa248be4af9cc2f3f26095ec34f21f78d201ea
alpetrushkin <alpetrushkin1984@mail.ru>
2023-03-07 22:13:49 +0300

    Создаём компонент, SCSS, import/export, props (пропсы)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import '../src/scss/app.scss';
+import {Header} from "./components/header/Header";
+import {Categories} from "./components/categories/Categories";
+import {Sort} from "./components/sort/Sort";
+import {PizzaBlock} from "./components/pizzaBlock/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+   return (
+      <div className="wrapper">
+         <Header/>
+         <div className="content">
+            <div className="container">
+               <div className="content__top">
+                  <Categories/>
+                  <Sort/>
+               </div>
+               <h2 className="content__title">Все пиццы</h2>
+               <div className="content__items">
+                  <PizzaBlock title="Чизбургер-пицца" price={395}/>
+                  <PizzaBlock title="Сырная" price={450}/>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
