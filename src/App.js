916c70eb5fcc826da2e78e575863151a8f0939c8
alpetrushkin <alpetrushkin1984@mail.ru>
2023-06-21 06:05:39 +0300

    Revert "error"
    This reverts commit 90c24c946b7272919572c2efe7e2001253e4c350.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import '../src/scss/app.scss';
 import {Header} from "./components/header/Header";
 import {Home} from "./components/Pages/Home";
 import {NotFound} from "./components/Pages/NotFound";
 import {Route, Routes} from "react-router-dom";
 import {Cart} from "./components/Pages/Cart";
-
-import '../src/scss/app.scss';
 
 export const SearchContext = React.createContext('');
 
@@ -14,16 +13,16 @@ function App() {
 
    return (
       <div className="wrapper">
-        <SearchContext.Provider value={{searchActive, setSearchActive}}>
-          <Header/>
-          <div className="content">
-            <Routes>
-              <Route path={'/'} element={<Home/>}/>
-              <Route path={'/cart/'} element={<Cart/>}/>
-              <Route path={'*'} element={<NotFound/>}/>
-            </Routes>
-          </div>
-        </SearchContext.Provider>
+         <SearchContext.Provider value={{ searchActive, setSearchActive }}>
+            <Header/>
+            <div className="content">
+               <Routes>
+                  <Route path={'/'} element={<Home/>}/>
+                  <Route path={'/cart/'} element={<Cart/>}/>
+                  <Route path={'*'} element={<NotFound/>}/>
+               </Routes>
+            </div>
+         </SearchContext.Provider>
       </div>
    );
 }
